e36b50e26cdb30f58b2df567d42ff4e53e982e2d
Polarish <72297937+PolarishT@users.noreply.github.com>
2024-04-17 17:51:41 +0800

    fix(#3): rebuild ci check_commit_msg

--- scripts/check_commit_msg.sh
+++ scripts/check_commit_msg.sh
@@ -2,7 +2,7 @@
 MSG=`awk '{printf("%s",$0)}' $1`
     echo -e "\033[32m start verify commit-msg..."
 
-if [[ $MSG =~ ^(Feature|BugFix|UnitTest|Docs|Style|Refactor|Chore|CI|Build|Revert)\(.*\):.*$ ]]
+if [[ $MSG =~ ^(feat|fix|docs|style|refactor|perf|test|chore|ci|build|revert)\(.*\):.*$ ]]
 then
 	echo -e "\033[32m 🚀commit check success!!! \033[0m\n"
 else
